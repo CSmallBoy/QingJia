b227e8bb63dc03f9ec896a5fde47d06976f54373
SmallBoy <834550465@qq.com>
2016-02-16 16:13:38 +0800

    left 合并家庭的滑动动画 以及标签详情的紧急联系人

---
+++ Project/Classes/Main/Left/Controller/HCMergingFamilyViewController.m
@@ -0,0 +1,37 @@
+//
+//  HCMergingFamilyViewController.m
+//  Project
+//
+//  Created by 朱宗汉 on 16/2/15.
+//  Copyright © 2016年 com.xxx. All rights reserved.
+//
+
+#import "HCMergingFamilyViewController.h"
+
+@interface HCMergingFamilyViewController ()
+
+@end
+
+@implementation HCMergingFamilyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
