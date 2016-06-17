321b7b4e728adb61df00857099cfc625ec5ea860
Chance <caiqingwang24@163.com>
2016-06-17 17:40:07 +0800

    推送修复

---
+++ Project/Classes/Main/Network/More/Promised /HCScanCardApi.m
@@ -0,0 +1,13 @@
+//
+//  HCScanCardApi.m
+//  钦家
+//
+//  Created by Tony on 16/6/17.
+//  Copyright © 2016年 com.xxx. All rights reserved.
+//
+
+#import "HCScanCardApi.h"
+
+@implementation HCScanCardApi
+
+@end
